f6baf2ac6ff9398b5d9be72db979132024afb066
Peter <ppapadim12@gmail.com>
2017-08-30 23:29:47 -0400

    user creation is complete. Route for post request complete.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.find_by(fb_id: params[:userID])
+    if !@user
+      @user = User.create(fb_id: params[:userID], first_name: params[:firstName], last_name: params[:lastName], email: params[:email])
+    end
+    render json: @user.id
+  end
+
+end
